d3b64d43ca6b4bde1a6b78fad89177cb715bde4a
mr_eugene <roberttimethone@gmail.com>
2025-11-28 21:26:59 +0100

    sol

--- cinema/tests/test_views.py
+++ cinema/tests/test_views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from cinema.models import Movie, Genre, Actor
-from cinema.serializers import MovieSerializer, MovieListSerializer
+from cinema.serializers import MovieListSerializer
 
 
 MOVIE_URL = reverse("cinema:movie-list")
